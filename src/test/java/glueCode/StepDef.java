98dcbb904be941f0d1aa648bab0ecdf2302fe3ee
Samresh Bhardwaj <Samresh.bhardwaj@Gmail.com>
2021-06-08 13:11:45 +1000

    Update StepDef.java

--- src/test/java/glueCode/StepDef.java
+++ src/test/java/glueCode/StepDef.java
@@ -1,6 +1,5 @@
 package glueCode;
 
-
 import com.jayway.restassured.response.Response;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -26,7 +25,23 @@ public class StepDef extends Utils {
         setBaseURI();
 
         // Set Base path as country ISO code
-        //		setBasePath(arg1);
+        //    setBasePath(arg1);
+        Utils.path = arg1;
+
+        //Get response
+        response = getResponse();
+        System.out.println("************The Response value as --" + response.prettyPrint());
+    }
+
+    @Given("^To initiate Rest service to get country details with name as \"([^\"]*)\"$")
+    public void to_initiate_Rest_service_to_get_country_details_with_name_as(String arg1) throws Throwable {
+        // Set Request parameters
+
+        // Set Base URI
+        setBaseURI();
+
+        // Set Base path as country ISO code
+        //    setBasePath(arg1);
         Utils.path = arg1;
 
         //Get response
@@ -40,9 +55,15 @@ public class StepDef extends Utils {
         // To verify the response status code
         assertEquals("Status Check Failed!", 200, response.getStatusCode());
 
+    }
 
+    @Then("^Error status should be \"([^\"]*)\"$")
+    public void Error_status_should_be(Integer arg1) throws Throwable {
+        // To verify the response status code
+        assertEquals("Status Check Passed!", 404, response.getStatusCode());
 
     }
+
 
     @Then("^response should includes the following$")
     public void response_includes_the_following(DataTable arg1) throws Throwable {
@@ -50,11 +71,9 @@ public class StepDef extends Utils {
         // This method to verify response body
         List < List < String >> data = arg1.raw();
         System.out.println("*************** Actual Data Table - data value as-- " + data);
-
-        response.then().body("RestResponse.result.name", equalTo(data.get(1).get(0)));
-        response.then().body("RestResponse.result.alpha2_code", equalTo(data.get(1).get(1)));
-        response.then().body("RestResponse.result.alpha3_code", equalTo(data.get(1).get(2)));
-
+        response.then().body("name", equalTo(data.get(1).get(0)));
+        response.then().body("capital", equalTo(data.get(1).get(1)));
+        response.then().body("region", equalTo(data.get(1).get(2)));
 
     }
 
